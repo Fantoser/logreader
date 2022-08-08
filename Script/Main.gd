c5124902fb89830e824500cc084c3a2bf4b354a0
Fantoser <k.fantoser@gmail.com>
2022-08-08 19:06:53 +0200

    Implemented handling and saving every IC message and movement and character and area into a custom resource file

--- Script/Main.gd
+++ Script/Main.gd
@@ -1,47 +1,120 @@
 extends Node
 
-
-var logFile
-
+var session = Session.new()
+var astring: String
+var placeless_messages = {}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	get_viewport().connect("size_changed", self, "_resize")
+
+func _resize():
+	self.rect_size = get_viewport().size
 
 func _on_FileLoad_pressed():
 	var selector = FileDialog.new()
 	selector.add_filter("*.log")
 	selector.set_mode(selector.MODE_OPEN_FILE)
 	selector.set_access(selector.ACCESS_FILESYSTEM)
+	selector.current_dir = OS.get_system_dir(OS.SYSTEM_DIR_DESKTOP)
 	self.add_child(selector)
 	selector.popup_centered_clamped(Vector2(600,400))
-	logFile = yield(selector,"file_selected")
-	get_node("%FilePathField").text = logFile
+	get_node("%FilePathField").text = yield(selector,"file_selected")
 
 func _on_Read_pressed():
-	get_node("%LoadingLabel").visible = true
-	wait(0.1)
-	var file = File.new()
-	if file.open(logFile,file.READ) == OK:
-		var index = 1
-		while not file.eof_reached(): # iterate through all lines until the end of file is reached
-			var line = file.get_line()
-			line += "\n"
-			$PanelContainer2/RichTextLabel.text += line
-			index += 1
-		file.close()
-	get_node("%LoadingLabel").visible = false
+	if get_node("%FilePathField").text == "":
+		get_node("%LoadingLabel").text = "No file selected"
+	else:
+		get_node("%LoadingLabel").text = "Loading..."
+		var t = Timer.new()
+		t.set_wait_time(0.1)
+		t.set_one_shot(true)
+		self.add_child(t)	#7 line of code cause there is no wait() function WOOO
+		t.start()
+		yield(t, "timeout")
+		t.queue_free()
 
-func wait(seconds):
-	var t = Timer.new()
-	t.set_wait_time(seconds)
-	t.set_one_shot(true)
-	self.add_child(t)
-	t.start()
-	yield(t, "timeout")
-	t.queue_free()
+		var file = File.new()
+		if file.open(get_node("%FilePathField").text,file.READ) == OK:
+			var index = 1
+			while not file.eof_reached(): # iterate through all lines until the end of file is reached
+				var line = file.get_line()
+				if line.find("GMT]") != -1: #Check if a timestamped line, to avoid login messages and such
+					if line.find("[OOC]") == -1: #Check if message is not an OOC message, so it have to be IC
+						_save_IC_message(line)
+						continue
+					if line.find("moves from") != -1:
+						_save_movement(line)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
+				index += 1
+			file.close()
+		$PanelContainer2/RichTextLabel.text = astring
+		ResourceSaver.save("res://test_save.tres", session)
+		get_node("%LoadingLabel").text = ""
+
+func _save_character(name):
+	session.characters[name] = {
+		"Color": Color(255, 255, 255, 255),
+		"location": null
+	}
+
+func _save_IC_message(line):
+#	[Sat Aug 6 18:21:36 2022 GMT] Fawkes (Kazuichi): }}}[6] {{{The ribbon. Yeah, that interests me.
+	var lineArray = line.split(" ")
+	var time = lineArray[3]
+	var character = lineArray[6].rstrip(":")
+	var message = line.split(":", true, 3)[3]
+	if message.find("] {{{") != -1 and message.find(" }}}[") != -1:
+		message = message.split("] {{{", true, 1)[1]
+	if session.characters.has(character) == false:
+		_save_character(character)
+		placeless_messages[character] = []
+	if session.characters[character]["location"] == null: #Character's location unknown
+		placeless_messages[character].append({
+			"time": time,
+			"message": message
+		})
+	else:
+		var location = session.characters[character]["location"]
+		session.messages[location][time] = {
+			"speaker": character,
+			"message": message
+		}
+
+func _save_movement(line):
+#	[OOC][Sat Aug 6 18:38:41 2022 GMT] $H: [8] Willow moves from [52] Security Room to [51] Downstairs Lobby.
+	var character = line.split("]")[3].lstrip(" ").split("moves from")[0].rstrip(" ")
+	var from = line.split("] ")[3].split(" to")[0]
+	var fromID = line.split("] ")[2].split("[")[1]
+	var to = line.split("] ")[4].rsplit(".")[0]
+	var toID = line.split("] ")[3].split("[")[1]
+	var time = line.split(" ")[3]
+#	astring += line + "\n" + time + "\n_________________\n"
+	session.characters["location"] = toID
+	# Save from and to areas if they are not saved yet
+	if session.areas.has(from) == false:
+		session.areas[fromID] = {
+			"name": from
+		}
+	if session.areas.has(to) == false:
+		session.areas[toID] = {
+			"name": to
+		}
+	# Save the messages what had unknown location
+	if placeless_messages.has(character):
+		for message in placeless_messages[character]:
+			if session.messages.has(fromID) == false:
+				session.messages[fromID] = {}
+			session.messages[fromID][message["time"]] = {
+			"speaker": character,
+			"message": message["message"]
+		}
+	# Save movement
+	if session.movements.has(time) == false:
+		session.movements[time] = []
+	session.movements[time].append({
+		"character": character,
+		"from": fromID,
+		"to": toID
+	})
--- Script/Session_resource.gd
+++ Script/Session_resource.gd
@@ -1,16 +1,10 @@
 extends Resource
 class_name Session
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export(Dictionary) var areas = {}
 
+export(Dictionary) var characters = {}
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+export(Dictionary) var messages = {}
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+export(Dictionary) var movements = {}
